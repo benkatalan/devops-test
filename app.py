55f39794ca9bf152ef4262a7f3d45a87e9852463
Ben Katalan <you@example.com>
2025-09-10 14:52:51 +0300

    change hello

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.get('/')
 def home():
-    return "hello from docker"
+    return "hello"
 #ben the king
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000)
